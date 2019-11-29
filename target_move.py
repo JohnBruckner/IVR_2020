b279f5d33b636155c420c15ebf925a0cada167fe
Connor Cleary <connorc204@gmail.com>
2019-11-29 02:22:41 +0000

    update joints function

--- target_move.py
+++ target_move.py
@@ -7,17 +7,18 @@ from std_msgs.msg import String
 from std_msgs.msg import Float64
 
 # Calculate the forward kinematics
-def forward_kinematics(self, image): 
-  joints - self.detect_joint_angles(image) # or other method
-  end_effector = np.array([-2*np.cos(t_1)*np.cos(t_2)*np.cos(t_3)*np.cos(t_4) - 2*np.cos(t_4)*np.sin(t_1)*np.sin(t_3) + 2*np.cos(t_1)*np.sin(t_2)*np.sin(t_4)-3*np.cos(t_1)*np.cos(t_2)*np.cos(t_3)- 3*np.sin(t_1)*np.sin(t_3),
-                  -2*np.cos(t_2)*np.cos(t_3)*np.cos(t_4)*np.sin(t_1)+2*np.cos(t_1)*np.cos(t_4)*np.sin(t_3)+2*np.sin(t_1)*np.sin(t_4)-3*np.sin(t_1)*np.cos(t_2)*np.cos(t_3)+3*np.cos(t_1)*np.sin(t_3),
-                  -2*np.cos(t_3)*np.cos(t_4)*np.sin(t_2)-2*np.cos(t_2)*np.sin(t_4)-3*np.cos(t_3)*np.sin(t_2)+2])
-  return end_effector 
+def forward_kinematics(x): 
 
+  end_effector = np.array([-2*np.cos(x[0])*np.cos(x[1])*np.cos(x[2])*np.cos(x[3]) - 2*np.cos(x[3])*np.sin(x[0])*np.sin(x[2]) + 2*np.cos(x[0])*np.sin(x[1])*np.sin(x[3])-3*np.cos(x[0])*np.cos(x[1])*np.cos(x[2])- 3*np.sin(x[0])*np.sin(x[2]),
+                  -2*np.cos(x[1])*np.cos(x[2])*np.cos(x[3])*np.sin(x[0])+2*np.cos(x[0])*np.cos(x[3])*np.sin(x[2])+2*np.sin(x[0])*np.sin(x[3])-3*np.sin(x[0])*np.cos(x[1])*np.cos(x[2])+3*np.cos(x[0])*np.sin(x[2]),
+                  -2*np.cos(x[2])*np.cos(x[3])*np.sin(x[1])-2*np.cos(x[1])*np.sin(x[3])-3*np.cos(x[2])*np.sin(x[1])+2])
+
+  return end_effector 
+  
 # Calculate Jacobian
 def calculate_jacobian(self, image):
     joints - self.detect_joint_angles(image) # or other method
-    [t_1, t_2, t_3, t_4] = joints
+    [x[0], t_2, x[2], t_4] = joints
     jacobian = np.array([[2*np.sin(t_1)*np.cos(t_2)*np.cos(t_3)*np.cos(t_4) - 2*np.cos(t_4)*np.cos(t_1)*np.sin(t_3) - 2*np.sin(t_1)*np.sin(t_2)*np.sin(t_4) + 3*np.sin(t_1)*np.cos(t_2)*np.cos(t_3) - 3*np.cos(t_1)*np.sin(t_3),
                   2*np.cos(t_1)*np.sin(t_2)*np.cos(t_3)*np.cos(t_4) + 2*np.cos(t_1)*np.cos(t_2)*np.sin(t_4) + 3*np.cos(t_1)*np.sin(t_2)*np.cos(t_3),
                   2*np.cos(t_1)*np.cos(t_2)*np.sin(t_3)*np.cos(t_4) - 2*np.cos(t_4)*np.sin(t_1)*np.cos(t_3) + 3*np.cos(t_1)*np.cos(t_2)*np.sin(t_3) - 3*np.sin(t_1)*np.cos(t_3)],
@@ -41,8 +42,6 @@ def move():
   robot_joint2_pub = rospy.Publisher("/target/y_position_controller/command", Float64, queue_size=10)
   robot_joint3_pub = rospy.Publisher("/target/z_position_controller/command", Float64, queue_size=10)
   robot_joint4_pub = rospy.Publisher("/target2/x2_position_controller/command", Float64, queue_size=10)
-  robot_joint5_pub = rospy.Publisher("/target2/y2_position_controller/command", Float64, queue_size=10)
-  robot_joint6_pub = rospy.Publisher("/target2/z2_position_controller/command", Float64, queue_size=10)
   t0 = rospy.get_time()
   while not rospy.is_shutdown():
     cur_time = np.array([rospy.get_time()])-t0
@@ -62,17 +61,16 @@ def move():
     x_d = 2* np.cos(cur_time * np.pi/5)
     y_d = 2* np.sin(cur_time * np.pi/5)
     joint4=Float64()
-    joint4.data=  x_d
-    joint5=Float64()
-    joint5.data=  y_d
-    joint6=Float64()
-    joint6.data= 5
+    joint4.data = x_d
     robot_joint4_pub.publish(joint4)
-    robot_joint5_pub.publish(joint5)
-    robot_joint6_pub.publish(joint6)
     rate.sleep()
 
+def callback():
+ 
 
+rospy.init_node("target_move", anonymous=True)
+joints_sub = message_filters.Subscriber("/joint_states'", String)
+pos_sub = message_filters.Subscriber("/sphere_position", String)
 
 # run the code if the node is called
 if __name__ == '__main__':
